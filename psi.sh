14edcc2da4cc6a54d4703364d08baaa764c6464e
Brandon Stephens <bstephens@doeanderson.com>
2016-05-27 15:20:54 -0400

    [add] init commit based off pa11y-machine

---
+++ psi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#set -o nounset
+#set -o errexit
+
+LOCAL_ROOT=`pwd`
+REPORT_DIR=reports
+
+# make dir if it doesn't exist
+if [ ! -d "$LOCAL_ROOT/$REPORT_DIR" ]; then
+  mkdir "$LOCAL_ROOT/$REPORT_DIR"
+fi
+
+START=$(date +"%s")
+START_DIR=$REPORT_DIR/`date '+%Y-%m-%d_%H.%M.%S'`
+
+# append a line return to make sure last line is processed
+sed -i '' -e '$a\' $LOCAL_ROOT/urls.txt
+
+mkdir $START_DIR
+cd $START_DIR
+
+while read line; do psi --format=json "$line"; done < $LOCAL_ROOT/urls.txt >> "$line".html
+
+cd $LOCAL_ROOT
+
+mv $LOCAL_ROOT/urls.txt $START_DIR
+
+END=$(date +"%s")
+DIFF=$(($END-$START))
+
+echo -e "Reports took $(($DIFF / 60))min $(($DIFF % 60))sec"
---
+++ readme.md
@@ -0,0 +1,15 @@
+# PSI Machine
+
+Check a list of urls against Google PageSpeed Index API.
+
+## Installation
+
+1. `npm install`
+
+## Use
+
+1. add a `urls.txt` file to the `psi-machine` directory
+2. cd to `psi-machine`
+3. run `. psi.sh`
+
+## Backlog
